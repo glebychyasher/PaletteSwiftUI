27a8490b6a3e5d86a42014eecc0f671ac1af8a67
glebychyasher <zobningleb01@icloud.com>
2023-12-17 23:26:05 +0300

    Initial Commit

---
+++ PaletteSwiftUI/PaletteSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaletteSwiftUIApp.swift
+//  PaletteSwiftUI
+//
+//  Created by Глеб Зобнин on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct PaletteSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
